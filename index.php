920952d458bef2f9aa384c153e87ee845ce81981
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-20 15:00:45 +0100

    STYLE modificato stile

--- index.php
+++ index.php
@@ -4,35 +4,35 @@
 
         [
             'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
+            'description' => 'Completamente ristrutturato con materiali di alta qualità nel corso degli ultimi anni, Hotel Belvedere Scuol è collegato tramite corridoi al centro benessere Bogn Engiadina e a 4 ristoranti',
             'parking' => true,
             'vote' => 4,
             'distance_to_center' => 10.4
         ],
         [
             'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
+            'description' => 'Central Park Resident 2022 Opening is a 4-star property situated in Bishkek. The accommodation features a 24-hour front desk, airport transfers, luggage storage space and free WiFi.',
             'parking' => true,
             'vote' => 2,
             'distance_to_center' => 2
         ],
         [
             'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
+            'description' => 'Hotel Rivamare is set directly on the seafront. Breakfast is served on a spacious terrace with beautiful sea views, and free parking is available on site.',
             'parking' => false,
             'vote' => 1,
             'distance_to_center' => 1
         ],
         [
             'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
+            'description' => 'Hotel Bellavista is set in the heart of Cavalese and offers beautiful views of the Lagorai Mountains. It features a modern wellness center with hot tub, sauna, and steam bath.',
             'parking' => false,
             'vote' => 5,
             'distance_to_center' => 5.5
         ],
         [
             'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
+            'description' => 'Just a 10-minute walk from Verona Arena and Porta Nuova Station, Hotel Siena provides a convenient central location in Verona. It features elegant air-conditioned rooms with free Wi-Fi.',
             'parking' => true,
             'vote' => 2,
             'distance_to_center' => 50
@@ -58,20 +58,23 @@
 
 <!--2-Mostrare le informazioni con una tabella. -->
 
- <div class="container">
+ <div class="container mt-5 pt-5">
+
+    <h1 class="text-danger">Welcome to Italy!</h1>
+    <h3 class="mb-5 text-success">Find the hotel that's right for you</h3>
 
    <div class="row">
 
-    <div class="col-11">
+    <div class="col-12">
 
       <table class="table table-striped table-dark">
         <thead>
           <tr>
              <th>Name:</th>
              <th>Description:</th>
-             <th>City:</th>
-             <th>Country:</th>
-             <th>Price:</th>
+             <th>Parking:</th>
+             <th>Vote:</th>
+             <th>Distance to the center:</th>
          </tr>
        </thead>
 
@@ -83,9 +86,9 @@
               <tr>
                  <td><?= $hotel['name'] ?></td>
                  <td><?= $hotel['description'] ?></td>
-                 <td><?= $hotel['city'] ?></td>
-                 <td><?= $hotel['country'] ?></td>
-                 <td><?= $hotel['price_per_night'] ?>€</td>
+                 <td><?= $hotel['parking'] ?></td>
+                 <td><?= $hotel['vote'] ?></td>
+                 <td><?= $hotel['distance_to_center'] ?>km</td>
                </tr>
 
             <?php endforeach; ?>
