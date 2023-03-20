9f78ad8b0d1bca0499bfbd3d9deb27728a748213
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-20 14:05:07 +0100

    EDIT aggiunto array Hotel

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+// array di hotel
+$hotels = [
+    [
+        "name" => "Hotel Belvedere",
+        "description" => "Situato a pochi passi dal mare, l'Hotel Belvedere offre camere con balcone e vista sul mare.",
+        "city" => "Rimini",
+        "country" => "Italia",
+        "price_per_night" => 80.00,
+        "stars" => 3
+    ],
+    [
+        "name" => "Hotel Moderno",
+        "description" => "L'Hotel Moderno si trova nel centro della città, a pochi passi dai principali punti di interesse.",
+        "city" => "Firenze",
+        "country" => "Italia",
+        "price_per_night" => 120.00,
+        "stars" => 4
+    ],
+    [
+        "name" => "Hotel Excelsior",
+        "description" => "Situato di fronte alla spiaggia, l'Hotel Excelsior offre camere con vista sul mare e una piscina all'aperto.",
+        "city" => "Cagliari",
+        "country" => "Italia",
+        "price_per_night" => 150.00,
+        "stars" => 5
+    ]
+];
+?>
