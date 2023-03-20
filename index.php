75654304bb4b48ecaebc0a9f242adb4de0b42bd2
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-20 14:30:48 +0100

    EDIT aggiunto ciclo foreach

--- index.php
+++ index.php
@@ -1,29 +1,63 @@
 <?php
-// array di hotel
+
+// elenco hotel-array
 $hotels = [
     [
         "name" => "Hotel Belvedere",
-        "description" => "Situato a pochi passi dal mare, l'Hotel Belvedere offre camere con balcone e vista sul mare.",
-        "city" => "Rimini",
-        "country" => "Italia",
-        "price_per_night" => 80.00,
-        "stars" => 3
+        "description" => "Hotel Belvedere is located in the heart of the city, just a few steps from the main square.",
+        "city" => "Rome",
+        "country" => "Italy",
+        "price_per_night" => 120
     ],
     [
-        "name" => "Hotel Moderno",
-        "description" => "L'Hotel Moderno si trova nel centro della città, a pochi passi dai principali punti di interesse.",
-        "city" => "Firenze",
-        "country" => "Italia",
-        "price_per_night" => 120.00,
-        "stars" => 4
+        "name" => "Grand Hotel",
+        "description" => "Grand Hotel is a luxury hotel located in the historic center of Florence.",
+        "city" => "Florence",
+        "country" => "Italy",
+        "price_per_night" => 300
     ],
     [
-        "name" => "Hotel Excelsior",
-        "description" => "Situato di fronte alla spiaggia, l'Hotel Excelsior offre camere con vista sul mare e una piscina all'aperto.",
-        "city" => "Cagliari",
-        "country" => "Italia",
-        "price_per_night" => 150.00,
-        "stars" => 5
+        "name" => "Hotel Plaza",
+        "description" => "Hotel Plaza is a modern hotel located in the financial district of Milan.",
+        "city" => "Milan",
+        "country" => "Italy",
+        "price_per_night" => 150
     ]
 ];
+
+//1-Stampare in pagina la lista hotel
+
+foreach ($hotels as $hotel): 
+    
+  ?>
+     <h2><?= $hotel['name'] ?></h2>
+      <p><?= $hotel['description'] ?></p>
+      <h3><?= $hotel['city'] ?></h3>
+      <p><?= $hotel['country'] ?></p>
+      <h4>Prezzo a notte: <?= $hotel['price_per_night'] ?>€</h4>
+  
+  <?php
+  endforeach;
+
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HOTEL PHP</title>
+  <link rel="stylesheet" href="./css/app.css">
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
